7fbfd78355c81b03144d9395d490649db842778c
Bruno Milare <milare@gmail.com>
2013-11-09 12:55:18 -0200

    my sticker col

--- models/album.js
+++ models/album.js
@@ -9,4 +9,23 @@ function Album(attributes) {
   this.height = attributes.height || AlbumDefaultHeight;
 }
 
+Album.prototype.findStickerByNumber = function(number) {
+  var stickers = this.getAllStickers();
+  for(var i=0; i < stickers.length; i++) {
+    if(stickers[i].number == number) { return stickers[i]; }
+  }
+  return null;
+}
+
+Album.prototype.getAllStickers = function() {
+  var stickers = []; 
+  for(var i=0; i < this.pages.length; i++) {
+    var page = this.pages[i];
+    for(var j=0; j < page.stickers.length; j++) { 
+      stickers.push(page.stickers[j]);
+    }
+  }
+  return stickers;
+};
+
 module.exports = Album;
